3c64d02448a98470ca7475e48ea26fcf5d4451c5
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-03-30 22:52:11 +0200

    set up the database

--- app.py
+++ app.py
@@ -2,16 +2,19 @@ from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
 #import SQLALchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 load_dotenv()
 
 #set the SQLALCHEMY_DATABASE_URI key
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 #create an SQLAlchemy object named `db` and bind it to your app
-
+db = SQLAlchemy()
+db.init_app(app)
 #a simple initial greeting
 @app.route('/')
 @app.route('/index')
