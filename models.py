9f55e6c9fa0eddfdf3324af423fb1c4390164489
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-03-30 23:12:14 +0200

    create the Song model

--- models.py
+++ models.py
@@ -1,26 +1,40 @@
 from app import app, db
-from sqlalchemy import String, ForeignKey
+from sqlalchemy import String, ForeignKey, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 
 #the User model: each user has a username, and a playlist_id foreign key referring
 #to the user's Playlist  
 class User(db.Model):
-  id: Mapped[int] = mapped_column(primary_key = True)
-  username: Mapped[str] = mapped_column(String(50),index = True, unique = True, nullable=False) 
-  playlist_id: Mapped[int] = mapped_column(ForeignKey('playlist.id'))
-  
-  #representation method
-  def __repr__(self):
-        return "{}".format(self.username)
-  
-  def serialize(self):
-        return {
-            "id": self.id,
-            "username": self.username
-        }
+	id: Mapped[int] = mapped_column(primary_key = True)
+	username: Mapped[str] = mapped_column(String(50),index = True, unique = True, nullable=False) 
+	playlist_id: Mapped[int] = mapped_column(ForeignKey('playlist.id'))
+	
+	#representation method
+	def __repr__(self):
+		return "{}".format(self.username)
+	
+	def serialize(self):
+		return {
+			"id": self.id,
+			"username": self.username
+		}
 
 #create the Song model here + add a nice representation method
-    
+class Song(db.Model):
+	id: Mapped[int] = mapped_column(primary_key= True)
+	artist: Mapped[str] = mapped_column(String(120), index = True, nullable = False)
+	title: Mapped[str] = mapped_column(String(120), index = True, nullable = False)
+	n: Mapped[int] = mapped_column(Integer)
+		
+	def __repr__(self):
+		return f"{self.title} by {self.artist}"
+	
+	def serialize(self):
+		return {
+			"id": self.id,
+			"artist": self.artist,
+			"title": self.title
+		}
+	
 #create the Item model here + add a nice representation method
-    
 #create the Playlist model here + add a nice representation method
