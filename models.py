1b998b5fcea53c724376fceafe7c4822ab125c03
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-03-30 23:56:33 +0200

    create the Playlist model

--- models.py
+++ models.py
@@ -1,13 +1,16 @@
 from app import app, db
 from sqlalchemy import String, ForeignKey, Integer
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 #the User model: each user has a username, and a playlist_id foreign key referring
 #to the user's Playlist  
 class User(db.Model):
 	id: Mapped[int] = mapped_column(primary_key = True)
 	username: Mapped[str] = mapped_column(String(50),index = True, unique = True, nullable=False) 
-	playlist_id: Mapped[int] = mapped_column(ForeignKey('playlist.id'))
+	playlist_id: Mapped[int] = mapped_column(ForeignKey('playlist.id'), unique= True)
+
+	# Relationship One - One
+	playlist: Mapped["Playlist"] = relationship("Playlist", back_populates="user")
 	
 	#representation method
 	def __repr__(self):
@@ -16,7 +19,8 @@ class User(db.Model):
 	def serialize(self):
 		return {
 			"id": self.id,
-			"username": self.username
+			"username": self.username,
+			"playlist_id": self.playlist_id
 		}
 
 #create the Song model here + add a nice representation method
@@ -42,7 +46,20 @@ class Item(db.Model):
 	song_id: Mapped[int] = mapped_column(ForeignKey('song.id'))
 	playlist_id: Mapped[int] = mapped_column(ForeignKey('playlist.id'))
 
+	# Relationship Many - One
+	playlist: Mapped["Playlist"] = relationship("Playlist", back_populates="items")
+
 	def __repr__(self):
 		return f"{self.song_id} - {self.playlist_id}"
 	
 #create the Playlist model here + add a nice representation method
+class Playlist(db.Model):
+	id: Mapped[int] = mapped_column(primary_key= True)
+
+	# Relationship One - One
+	user: Mapped["User"] = relationship("User", back_populates="playlist", uselist=False)
+	# Relationship One - Many
+	items: Mapped[list["Item"]] = relationship("Item", back_populates="playlist", cascade="all, delete-orphan")
+
+	def __repr__(self):
+		return f"{self.user} - {self.id}"
