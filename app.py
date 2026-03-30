4f5a750cfead8b787383c423a91fbefd9eef1091
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-03-30 20:36:37 +0200

    add starter codebase

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
+#import SQLALchemy
+
+app = Flask(__name__)
+
+load_dotenv()
+
+#set the SQLALCHEMY_DATABASE_URI key
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+#create an SQLAlchemy object named `db` and bind it to your app
+
+#a simple initial greeting
+@app.route('/')
+@app.route('/index')
+def greeting():
+    return render_template('greeting.html')
+
+# app name 
+@app.errorhandler(404) 
+def not_found(e): 
+  return render_template("404.html") 
+
+#uncomment the code below here when you are done creating database instance db and models
+#import routes
